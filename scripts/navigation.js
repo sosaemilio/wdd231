a7a8375c4c677a6a7d2d24384a79cbc8484bc052
Emilio Sosa <emiliossosa85@gmail.com>
2025-07-19 23:34:32 +0200

    mobile view

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,13 +1,17 @@
-const menubutton = document.querySelector("#menu-icon");
-const menuitems = document.querySelectorAll(".menu-item");
-// menuitems will be a Node List of the list items
+const menuIcon = document.getElementById("menu-icon");
 
-menubutton.addEventListener("click", () => {
-	if (menubutton.src.includes("images/menu.svg")) {
-		menuitems.forEach((item) => item.classList.toggle("menu-item"));
-		menubutton.src = "images/close-icon.svg";
-	} else {
-		menuitems.forEach((item) => item.classList.toggle("menu-item"));
-		menubutton.src = "images/menu.svg"
-	}
-});
+menuIcon.addEventListener("click", (e) => {
+    // Pick menu and display
+    const menuSelector = document.querySelector(".menu");
+    if (menuIcon.src.includes("images/menu.svg")) {
+        // Displays the menu
+        menuSelector.style.display = "flex";
+        // Update the src image to the close button
+        menuIcon.src = "../images/close.svg"
+    } else {
+        // Hides the menu
+        menuSelector.style.display = "none";
+        // Update the src image to the menu button
+        menuIcon.src = "../images/menu.svg"
+    }
+})
