a63527a2a34ff909bffe762c0ce8ae9c53fb227e
Emilio Sosa <emiliossosa85@gmail.com>
2025-08-08 05:22:02 +0200

    Fix current and modal

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -10,33 +10,30 @@ const goldButton = document.querySelector('.gold-button');
 
 const modal = document.getElementById('membership-benefits-box');
 const closeModal = document.getElementById('closeModal');
+const textContainer = document.getElementById('benefits-modal');
 
 nonprofitButton.addEventListener('click', () => {
-  modal.innerHTML = `
+  textContainer.innerHTML = `
     <h2>Non Profit Membership Benefits</h2>
     <p>No Fee</p>
-    <p>Only available for non profits organization in the metropolitan area</p>
-    <button id="closeModal">Close</button>
-  `;
+    <p>Only available for non profits organization in the metropolitan area</p> `;
   modal.showModal(); // display the modal dialog right away.
 });
 
 bronzeButton.addEventListener('click', () => {
-  modal.innerHTML = `
+  textContainer.innerHTML = `
     <h2>Bronze Membership Benefits</h2>
     <p>$30 / Month</p>
     <p>A good startpoint for small business and companies looking to see the perks of the chamber, our offer: </p>
     <ul>
         <li>Events Discounts</li>
         <li>Access to all the special Events</li>
-    </ul>
-    <button id="closeModal">Close</button>
-  `;
+    </ul> `;
   modal.showModal(); // display the modal dialog right away.
 });
 
 silverButton.addEventListener('click', () => {
-  modal.innerHTML = `
+  textContainer.innerHTML = `
     <h2>Silver Membership Benefits</h2>
     <p>$40 / Month </p>
     <p>The best choice for almost all business looking to expand and be part of the dynamic ecomony of the city.</p>
@@ -46,13 +43,12 @@ silverButton.addEventListener('click', () => {
         <li>Advertising</li>
         <li>Include the business logo into the banner and fliers of the Chamber</li>
         <li>Spotlight Position</li>
-    </ul>
-    <button id="closeModal">Close</button>`;
+    </ul>`;
         modal.showModal(); // display the modal dialog right away.
     });
 
 goldButton.addEventListener('click', () => {
-  modal.innerHTML = `
+  textContainer.innerHTML = `
     <h2>Gold Membership Benefits</h2>
     <p>$60 / Month</p>
     <p>For those looking to become partner and meeting the best of the industry</p>
@@ -63,14 +59,11 @@ goldButton.addEventListener('click', () => {
         <li>Inhouse and Online Training</li>
         <li>Discount in POS and accounting Softwares</li>
         <li>Business Consulting</li>
-    </ul>
-    <button id="closeModal">Close</button>
-
-`;
+    </ul>`;
     modal.showModal(); // display the modal dialog right away.
 });
 
-// Close the modal dialog when the close button is clicked
+    // Close the modal dialog when the close button is clicked
 // This is just an example of how to close the modal dialog
 closeModal.addEventListener('click', () => {
     modal.close();
