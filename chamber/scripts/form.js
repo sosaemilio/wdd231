c1d797097fa7b9cc15f01ed0eb6249b31594f0e9
Emilio Sosa <emiliossosa85@gmail.com>
2025-08-08 04:57:41 +0200

    Add modals

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -3,4 +3,23 @@ dateField.value = new Date().toISOString().slice(0, 16); // Set the current date
 
 // Display the membership modals
 
-const membershipButtons = document.querySelectorAll('.membership-button');
+const nonprofitButton = document.querySelector('.nonprofit-button');
+const bronzeButton = document.querySelector('.bronze-button');
+const silverButton = document.querySelector('.silver-button');
+const goldButton = document.querySelector('.gold-button');
+
+const modal = document.getElementById('membership-benefits-box');
+const closeModal = document.getElementById('closeModal');
+
+nonprofitButton.addEventListener('click', () => {
+  modal.innerHTML = `
+    <h2>Non Profit Membership Benefits</h2>
+    <button id="closeModal">Close</button>
+  `;
+  modal.showModal(); // display the modal dialog right away.
+});
+
+// Usually you will want to wait for a user action to show the modal dialog
+closeModal.addEventListener('click', () => {
+modal.close();
+});
